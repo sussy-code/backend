59ab9b48bda98165a9944d7699deb541280ceb10
mrjvs <jellevs@gmail.com>
2023-12-21 20:38:51 +0100

    Implement trusted cloudflare ips
    Co-authored-by: William Oldham <github@binaryoverload.co.uk>

--- src/config/fragments/docker.ts
+++ src/config/fragments/docker.ts
@@ -4,4 +4,8 @@ export const dockerFragment: FragmentSchema = {
   postgres: {
     connection: 'postgres://postgres:postgres@postgres:5432/postgres',
   },
+  ratelimits: {
+    enabled: true,
+    redisUrl: 'redis://redis:6379',
+  },
 };
