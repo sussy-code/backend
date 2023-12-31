0c3de831fb774ea70113f33b8ed45cfb449e95d1
caio_nogueira_27 <caio.nogueira@losch.lu>
2023-12-31 13:03:32 +0000

    update deprecated composer syntax; change body format in API request

--- src/services/captcha.ts
+++ src/services/captcha.ts
@@ -4,16 +4,14 @@ import { StatusError } from '@/services/error';
 export async function isValidCaptcha(token: string): Promise<boolean> {
   if (!conf.captcha.secret)
     throw new Error('isValidCaptcha() is called but no secret set');
+  const formData = new URLSearchParams();
+  formData.append('secret', conf.captcha.secret);
+  formData.append('response', token);
   const res = await fetch('https://www.google.com/recaptcha/api/siteverify', {
     method: 'POST',
-    body: JSON.stringify({
-      secret: conf.captcha.secret,
-      response: token,
-    }),
-    headers: {
-      'content-type': 'application/json',
-    },
+    body: formData,
   });
+
   const json = await res.json();
   return !!json.success;
 }
