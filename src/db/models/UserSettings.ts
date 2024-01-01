b2e0841d0b4f5f58ebbf6092ab5a762a39aad56f
qtchaos <72168435+qtchaos@users.noreply.github.com>
2024-01-01 19:46:46 +0200

    Swap to using ArrayType

--- src/db/models/UserSettings.ts
+++ src/db/models/UserSettings.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { ArrayType, Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity({ tableName: 'user_settings' })
 export class UserSettings {
@@ -14,7 +14,7 @@ export class UserSettings {
   @Property({ name: 'default_subtitle_language', nullable: true })
   defaultSubtitleLanguage?: string | null;
 
-  @Property({ name: 'proxy_urls', nullable: true, type: 'text[]' })
+  @Property({ name: 'proxy_urls', type: ArrayType, nullable: true })
   proxyUrls?: string[] | null;
 }
 
